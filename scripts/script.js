b69a6e58106a2c1fc1e336c3cb766e9e4f5b5b7a
Ingvar <jamessoer91@gmail.com>
2025-01-22 14:38:37 +0300

    adding popups

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const detailsButtons = document.querySelectorAll(".details-btn");
+  const popup = document.getElementById("popup");
+  const closeBtn = document.querySelector(".close-btn");
+
+  // Элементы внутри попапа
+  const popupTitle = document.getElementById("popup-title");
+  const popupDescription = document.getElementById("popup-description");
+  const popupPrice = document.getElementById("popup-price");
+
+  // Добавляем обработчик для каждой кнопки
+  detailsButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // Получаем данные из атрибутов
+      const title = btn.getAttribute("data-title");
+      const description = btn.getAttribute("data-description");
+      const price = btn.getAttribute("data-price");
+
+      // Заполняем попап данными
+
+      popupTitle.textContent = title;
+      // popupDescription.textContent = description;
+      popupDescription.innerHTML = description;
+      popupPrice.textContent = price;
+
+      // Показываем попап
+      popup.classList.add("show");
+    });
+  });
+
+  // Закрытие попапа
+  closeBtn.addEventListener("click", () => {
+    popup.classList.remove("show");
+  });
+
+  // Закрытие при клике вне попапа
+  popup.addEventListener("click", (event) => {
+    if (event.target === popup) {
+      popup.classList.remove("show");
+    }
+  });
+});
